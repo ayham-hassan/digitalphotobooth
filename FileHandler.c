72794170567d78a184d7630edeba1e8a69a83a93
dwiniarski <dwiniarski@4f1d6e44-3c74-11de-b418-9b7e54b75326>
2008-04-08 14:52:41 +0000

    Initial Rev.

---
+++ FileHandler.c
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * FileHandler.c
+ *
+ * 	 @authors -	David M. Winiarski - dmw1407@rit.edu
+ ******************************************************************************/
+
+#include "FileHandler.h"
+
+ /******************************************************************************
+ *
+ *  Function:       printImage
+ *  Description:    This function will print the image provided. 
+ *  Inputs:         toPrint - String of image to be printed.  
+ *  Outputs:       	retVal - Returns -1 if the image was not printed succesfully.
+							 Returns 0 if the image was printed successfully.  
+ *  Routines Called: execvp, waitpid
+ *
+ *****************************************************************************/
+int printImage(char * toPrint)
+{
+	int i, pid, childStatus, retVal;
+
+	/* Setup the argument strings. */
+	char cmd[4] = "lpr";
+	char o[3] = "-o";
+	char fit[8] = "fitplot";
+	
+	/* Set the arguments. */	
+	char * theArgs[5];
+	theArgs[0] = cmd;
+	theArgs[1] = o;
+	theArgs[2] = fit;
+	theArgs[3] = toPrint;
+	theArgs[4] = '\0';
+
+	/* Call ImageMagick to do the resizing. */
+	pid = fork();
+	if(pid < 0)
+	{ 
+		/* Couldn't fork... ERROR! */
+		retVal = -1;
+	} 
+	else if(pid == 0)
+	{ 
+		/* This is the child process. */
+		execvp(cmd, theArgs);
+		exit(1);
+	}
+	else
+	{
+		/* We are the parent process... wait for the first cmd. */
+		waitpid(pid,&childStatus,0);
+		if (WIFEXITED(childStatus) != 0)
+		{
+			retVal = OK_VAL;	
+		}
+		else
+		{
+			retVal = ERROR_VAL;
+		}
+	}
+}
+
---
+++ FileHandler.h
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * FileHandler.c
+ * 
+ * 	 @authors -	David M. Winiarski - dmw1407@rit.edu
+ ******************************************************************************/
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define MAX_SIZE 255
+
+#define ERROR_VAL -1
+#define OK_VAL 0
+
+extern int errno;
+/******************************************************************************
+ *
+ *  Function:       printImage
+ *  Description:    This function will print the image provided. 
+ *  Inputs:         toPrint - String of image to be printed.  
+ *  Outputs:       	retVal - Returns -1 if the image was not printed succesfully.
+							 Returns 0 if the image was printed successfully.  
+ *  Routines Called: execvp, waitpid
+ *
+ *****************************************************************************/
+int printImage(char * toPrint);
