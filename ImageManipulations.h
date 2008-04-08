466eeceba6cccf87ffcd6bce37d381c5406d3822
dwiniarski <dwiniarski@4f1d6e44-3c74-11de-b418-9b7e54b75326>
2008-04-08 14:17:13 +0000

    Initial Rev.

---
+++ ImageManipulations.h
@@ -0,0 +1,73 @@
+/*
+ * ImageManipulations.h
+ * 
+ *  Version:
+ * 	  	$Id$
+ * 
+ *  Revisions:
+ * 		$Log$
+ * 
+ * 	 @authors -	David M. Winiarski - dmw1407@rit.edu
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define MAX_SIZE 255
+
+#define ERROR_VAL -1
+#define OK_VAL 0
+ 
+/******************************************************************************
+ *
+ *  Function:       resizeImages
+ *  Description:    This function will create a two smaller copies of the
+ *					original photo.
+ *  Inputs:           
+ *  Outputs:         
+ *  Routines Called: 
+ *
+ *****************************************************************************/
+int resizeImages(char * inImage, char * smImage, char * lgImage);
+
+/******************************************************************************
+ *
+ *  Function:       createOilBlobImage
+ *  Description:    This function will create an oil blob version of the provided
+					image.
+ *  Inputs:           
+ *  Outputs:         
+ *  Routines Called: 
+ *
+ *****************************************************************************/
+int createOilBlobImage(char * inImage, char * oilBlobImage);
+
+/******************************************************************************
+ *
+ *  Function:       createCharcoalImage
+ *  Description:    This function will create a charcoal version of the provided
+					image.
+ *  Inputs:           
+ *  Outputs:         
+ *  Routines Called: 
+ *
+ *****************************************************************************/
+int createCharcoalImage(char * inImage, char * charcoalImage);
+
+/******************************************************************************
+ *
+ *  Function:       createTexturedImage
+ *  Description:    This function will create a texturized version of the provided
+					image.
+ *  Inputs:           
+ *  Outputs:         
+ *  Routines Called: 
+ *
+ *****************************************************************************/
+int createTexturedImage(char * inImage, char * texImage, char * texturedImage);
+
+
