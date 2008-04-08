67b6b4f3778d330ded706e03bae883fe196ee909
nprosser <nprosser@4f1d6e44-3c74-11de-b418-9b7e54b75326>
2008-04-08 14:59:11 +0000

    Updated Makefile to use the libjpeg.a in /usr/lib Updated cam.c to only take 10 adjustment frames Updated test.c to set the video format and resolution Added jpeglib.h so that the code should build independently of things outside this repo

--- trunk/proj-nprosser/test.c
+++ trunk/proj-nprosser/test.c
@@ -2,11 +2,22 @@
 #include "drv-v4l2.h"
 #include <stdio.h>
 
+/* Raw image format supported by QuickCam Pro 9000
+ * This code is defined in fourcc.c
+ */
+#define YUYV 0x56595559
+/* Low resolution video should be 640x480 */
+#define LR_WIDTH  640
+#define LR_HEIGHT 480
+
 int main(){
   V4L2Capture *capture = v4l2CaptureOpen("/dev/video0");
-  VidSize resolution;
-  v4l2CaptureGetResolution(capture, &resolution);
+  VidSize resolution, _resolution;
+  _resolution.width = LR_WIDTH;
+  _resolution.height = LR_HEIGHT;
+  v4l2CaptureSetImageFormat(capture, (fourcc_t)YUYV, &_resolution);
 
+  v4l2CaptureGetResolution(capture, &resolution);
   printf("Resolution is %dx%d \n", resolution.width, resolution.height);
   v4l2CaptureSetFPS(capture, 30);
   printf("Frame Rate is %f \n", v4l2CaptureGetFPS(capture));
@@ -16,7 +27,8 @@ int main(){
     return 1;
   }
 
-  v4l2CaptureRelease(capture);
+  v4l2CaptureStopStreaming(capture);
+  v4l2CaptureRelease(&capture);
 
   return 0;
 }
