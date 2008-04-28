8b5fd877f2a74a1698d9f7635d674e9ecdc074fe
dwiniarski <dwiniarski@4f1d6e44-3c74-11de-b418-9b7e54b75326>
2008-04-28 15:52:40 +0000

    Modified to use hacked button.

--- photobooth_start.sh
+++ photobooth_start.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+man #!/bin/bash
 
 ./photobooth &
 sleep 2
-joy2key "Digital Photo Booth" -dev /dev/input/js0 -X -buttons c -thresh 0 0 0 0 &
+joy2key "Digital Photo Booth" -dev /dev/input/js0 -X -buttons 0 0 0 0 0 0 0 0 0 0 0 0 0 0 c -thresh 0 0 0 0 &
 
